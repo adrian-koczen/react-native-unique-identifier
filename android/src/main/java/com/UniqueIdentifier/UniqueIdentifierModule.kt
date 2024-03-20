f719ffeb05dadf0cc9c6cdbc168ccbe293ff5e5d
adrian-koczen <koczenadrian@gmail.com>
2024-03-20 14:34:43 +0100

    feat: added function to fetch google email name on android device

--- android/src/main/java/com/UniqueIdentifier/UniqueIdentifierModule.kt
+++ android/src/main/java/com/UniqueIdentifier/UniqueIdentifierModule.kt
@@ -1,35 +1,58 @@
 package com.uniqueidentifier
 
-import android.Manifest
-import android.content.ContentResolver
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.provider.MediaStore
-import android.provider.OpenableColumns
-import android.util.Log
-import androidx.annotation.RequiresApi
+import android.accounts.AccountManager
+import android.app.Activity
+import android.content.Intent
+import com.facebook.react.bridge.BaseActivityEventListener
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.modules.core.PermissionAwareActivity
-import com.facebook.react.modules.core.PermissionListener
-import com.facebook.react.bridge.WritableNativeArray
-import com.facebook.react.bridge.WritableNativeMap
 
 class UniqueIdentifierModule(reactContext: ReactApplicationContext) : NativeModuleSpec(reactContext) {
-  override fun getName() = NAME
+    private val requestCode = 1
+    private var promise: Promise? = null
 
-  override fun requiresMainQueueSetup(): Boolean {
-    return true
-  }
+    init {
+        reactContext.addActivityEventListener(object : BaseActivityEventListener() {
+            override fun onActivityResult(activity: Activity?, code: Int, resultCode: Int, data: Intent?) {
+                if (code == requestCode && resultCode == Activity.RESULT_OK) {
+                    val accountName = data?.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
 
-  override fun getIcloudRecord(promise: Promise) {
-    promise.resolve("todo")
-  }
+                    promise?.resolve(accountName)
+                } else {
+                    promise?.reject("ERROR", "RESULT_CANCELED")
+                }
+            }
+        })
+    }
 
-  companion object {
-    const val NAME = "UniqueIdentifier"
-  }
+    override fun getName() = NAME
+
+    override fun requiresMainQueueSetup(): Boolean {
+        return false
+    }
+
+    override fun getIcloudRecord(promise: Promise) {
+        promise.reject("ERROR", "Not implemented")
+    }
+
+    override fun getAndroidAccountName(message: String, promise: Promise) {
+        this.promise = promise
+
+        val activity = reactApplicationContext.currentActivity
+        val intent = AccountManager.newChooseAccountIntent(
+            null,
+            null,
+            null,
+            message,
+            null,
+            null,
+            null
+        )
+
+        activity?.startActivityForResult(intent, requestCode)
+    }
+
+    companion object {
+        const val NAME = "UniqueIdentifier"
+    }
 }
